6a99ef259d6e93a04fdb8f26eb147a7dd1e7b3a8
jd356w-cloud <jd356w@gmail.com>
2025-09-27 19:15:40 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+motivation_profile/
+│
+├── app.py                # Your main Streamlit app
+├── requirements.txt      # List of dependencies
+└── README.md             # (Optional) Description of your app
